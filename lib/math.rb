ae58137a31b1e062542ec34329578a3ea1ce32d0
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-02 21:59:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,26 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
 end
+
+addition(2,2)
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
 end
 
-def division(num1, num2)
-
-end
+subtraction(3,2)
 
 def multiplication(num1, num2)
+  product = num1 * num2
+end
+
+multiplication(7,3)
+
+def division(num1, num2)
+  quotient = num2 / num1
+end
+
+division(4,12)
 
 end
 
@@ -19,5 +29,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
+  squareroot = 
 
 end
